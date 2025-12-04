11379381f15da80b452ea668ea3bfc415a9da3a8
Simon Gerwer <sge152147@stud.gibb.ch>
2025-12-04 13:54:19 +0100

    Add project files.

---
+++ TopCore2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TopCore2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TopCore2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TopCore2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
